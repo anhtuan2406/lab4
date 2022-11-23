6bf7858c522bb291536b52b21c68468a86842a44
anhtuan2406 <118682643+anhtuan2406@users.noreply.github.com>
2022-11-23 23:19:38 +0700

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -70,16 +70,3 @@ end
 
 -----------------
 
-DECLARE @tong1 int = 0,@d INT = 10,@F INT
-SET @F = 1
-WHILE (@f<=@d)
-BEGIN
-if (@f %2 =0)
-SET @tong1 = @tong1 + @f
-SET @f = @f + 1 
-if(@f = 4)
-SET @tong1 = @tong1 - 4
-END
-PRINT ('Ket qua la: ' )
-PRINT @tong1
-
